bc94bcef1329e531312b24c45bc2cdcb899f2b5f
Hajar Aswad <hajaraswadkom@gmail.com>
2023-01-21 07:31:52 +0700

    setup graphql

--- database/migrations/2023_01_21_002340_create_quests_table.php
+++ database/migrations/2023_01_21_002340_create_quests_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('quests', function (Blueprint $table) {
             $table->id();
             $table->text('title');
+            $table->text('description');
+            $table->integer('reward');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('quests');
     }
 };
